a646751d39ff033273318e1be4d82cf469260923
Vwasekar22 <58453391+Vwasekar22@users.noreply.github.com>
2020-08-31 19:00:46 +0530

    Update init.sql

--- init_sql_scripts/init.sql
+++ init_sql_scripts/init.sql
@@ -7,3 +7,6 @@ insert into country_and_capitals(country, capital) values ('India', 'Delhi');
 insert into country_and_capitals(country, capital) values ('China', 'Beijing');
 insert into country_and_capitals(country, capital) values ('Bhutan', 'Thimpu');
 insert into country_and_capitals(country, capital) values ('Russia', 'Moscow');
+insert into country_and_capitals(country, capital) values ('Australia', 'Canberra');
+insert into country_and_capitals(country, capital) values ('New Zealand', 'Wellington');
+insert into country_and_capitals(country, capital) values ('Japan', 'Tokyo');
